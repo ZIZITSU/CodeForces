93b2560b56409db458be616f39ec82cb40fe182b
ZIZITSU <mahirlabib389@gmail.com>
2026-04-02 04:12:35 +0600

    Till 2nd April

--- Codeforces/1475A.cpp
+++ Codeforces/1475A.cpp
@@ -1,13 +1,21 @@
 #include<iostream>
-
 using namespace std;
 
-int main(){
+int main() {
+    int t;
+    cin >> t;
 
-int x;
+    while (t--) {
+        long long x;
+        cin >> x;
 
+        while (x % 2 == 0) {
+            x /= 2;
+        }
 
-
-
-
+        if (x == 1)
+            cout << "No\n";
+        else
+            cout << "Yes\n";
+    }
 }
